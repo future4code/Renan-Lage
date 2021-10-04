076b6b3b4c7e3c8e64ccb14ee1fbd67275f5ac81
Renan Lage <renanlopresti@gmail.com>
2021-10-04 17:00:37 -0300

    aula variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,65 @@
+/*
+1. Será impresso: 
+10
+10 5
+
+2. Será impresso:
+10 10 10
+
+3. As 2 primeiras variaveis, p e t, pedirão informações do usuario, e a resposta serão as próprias variaveis.
+por último aparecerá um alerta com uma mensagem com a divisão dos numeros colocados em cada variavel anterior.
+poderia ser usado:
+let horasTrabalhadas
+let salarioDeUmDia
+*/
+
+
+/*
+1.
+let nome = "";
+let idade = "";
+console.log(typeof nome);
+console.log(typeof idade);
+
+// As duas deu string, pois as duplas aspas criam campos de texto, e mesmo estando vazias elas continuam sendo campos de texto.
+
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+// elas continuaram sendo strings, pois quando vc recebe a informação do usuario, como padrão elas serao campos de texto.
+
+console.log("Ola,",nome, "você tem", idade, "anos.");
+*/
+
+/*
+2.
+let pergunta1 = prompt("Você está usando roupa azul hoje?")
+let pergunta2 = prompt("você tomou café hoje de manha?")
+let pergunta3 = prompt("Gostou da aula de hoje?")
+
+console.log("Você está usando roupa azul hoje? -", pergunta1)
+console.log("você tomou café hoje de manha? -", pergunta2)
+console.log("Gostou da aula de hoje? -", pergunta3)
+*/
+
+/*
+3.
+let a = 10;
+let b = 25;
+let c = a;
+
+console.log("O novo valor de a é", b) // O novo valor de a é 25
+console.log("O novo valor de b é", c) // O novo valor de b é 10
+*/
+/*
+Desafio
+
+let a = Number(prompt("numero 1"));
+let b = Number(prompt("numero 2"));
+console.log(a + b);
+console.log(a * b);
+*/
+
+
+
