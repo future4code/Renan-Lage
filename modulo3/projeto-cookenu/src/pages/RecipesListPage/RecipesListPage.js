6bf647b3bc704a9cda96329b612c8687c2639f11
Renan Lage <renanlopresti@gmail.com>
2021-12-16 21:08:25 -0300

    pagina principal

--- modulo3/projeto-cookenu/src/pages/RecipesListPage/RecipesListPage.js
+++ modulo3/projeto-cookenu/src/pages/RecipesListPage/RecipesListPage.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import useProtectedPage from '../../hooks/useProtectedPage';
+import RecipeCard from '../../componets/RecipeCard/RecipeCard';
+import { RecipeListContainer, AddRecipeButton } from './RecipesListPage.style';
+import useRequestData from '../../hooks/useRequestData';
+import { BASE_URL } from '../../constants/urls'
+import AddIcon from '@mui/icons-material/Add';
 
 export default function RecipesListPage() {
  useProtectedPage()
+ const recipes = useRequestData([], `${BASE_URL}/recipe/feed`)
+
+ const recipesCards = recipes.map((recipe) => {
+  return (
+   <RecipeCard
+    key={recipe.recipe_id}
+    image={recipe.image}
+    title={recipe.title}
+    onClick={() => null}
+   />)
+ })
  return (
-  <div>
-   <h1>Todas as Receitas</h1>
-  </div>
+  <RecipeListContainer>
+   {recipesCards}
+   <AddRecipeButton
+   color={'primary'}>
+    <AddIcon />
+   </AddRecipeButton>
+  </RecipeListContainer>
  )
 }
