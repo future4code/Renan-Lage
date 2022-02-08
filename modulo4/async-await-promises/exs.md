406388c2bbb5cbcee2d7da3251a1125598569e02
Renan Lage <renanlopresti@gmail.com>
2022-02-07 21:54:04 -0300

    exs async await

---
+++ modulo4/async-await-promises/exs.md
@@ -0,0 +1,9 @@
+1 a) /subscribers
+b) Promise<any>
+
+2a) diferencia apenas na sintaxe, um a função e nomeada co function, e na arrow function e nomeada com const
+
+3a) não teremos erro na tipagem.
+b) fazemos esse map para garantir que a função retorne com a ordem certa, e passe algum erro se tiver algum problema no meio.
+
+4)Essa função e uma função nomeada, porque não é um método de retorno de informações.
---
+++ modulo4/async-await-promises/src/index.ts
@@ -0,0 +1,76 @@
+import { baseURL } from './baseURL';
+import axios from "axios"
+
+type user = {
+ id: string;
+ name: string;
+ email: string;
+}
+type newsType = {
+ title: string
+ content: string
+ date: number
+}
+
+const news: newsType = {
+ title: "Outra notícia",
+ content: "conteúdo",
+ date: Date.now()
+}
+
+// async function getSubscribers(): Promise<any[]> {
+//  const response = await axios.get(`${baseUrl}/subscribers`);
+//  return response.data;
+// };
+
+const getSubscribers = async (): Promise<user[]> => {
+ const response = await axios.get(`${baseURL}/subscribers`)
+ return response.data.map((res: any) => {
+  return {
+   id: res.id,
+   name: res.name,
+   email: res.email,
+  };
+ });
+}
+
+async function createNews(news: newsType): Promise<void> {
+ await axios.put(`${baseURL}/news`, news);
+}
+
+
+const sendNotifications = async (
+ users: user[],
+ message: string
+): Promise<void> => {
+
+ try {
+  for (const user of users) {
+   await axios.post(`${baseURL}/notifications`, {
+    subscriberId: user.id,
+    message
+   });
+  }
+
+  console.log("All notifications sent");
+ } catch {
+  console.log("Error");
+ }
+};
+
+const main = async (): Promise<void> => {
+ try {
+
+  await createNews(news)
+
+  const subscribers = await getSubscribers()
+  console.log(subscribers)
+
+  await sendNotifications(subscribers, "seja bem vindo")
+
+ } catch (err: any) {
+  console.log(err.response?.data || err.message)
+ }
+}
+
+main()
