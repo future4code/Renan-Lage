92a201ffa7839191e50a3d84a01458be80bec3fb
Renan Lage <renanlopresti@gmail.com>
2022-03-14 23:58:43 -0300

    adicionando getPosts

--- modulo5/LaBook/src/index.ts
+++ modulo5/LaBook/src/index.ts
@@ -3,8 +3,10 @@ import signup from "./endpoints/signup"
 import login from "./endpoints/login"
 import createPost from "./endpoints/createPost"
 import getPostsById from "./endpoints/getPostsById"
+import getPosts from "./endpoints/getPosts"
 
 app.post('/users/signup', signup)
 app.post('/users/login', login)
 app.post('/posts/create', createPost)
 app.get('/posts/:id', getPostsById)
+app.get('/posts', getPosts)
